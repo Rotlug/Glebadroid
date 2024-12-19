0e1bf721906a74079d36ea705b9eee53c4ea1765
rotlug <rotlug77@gmail.com>
2024-12-19 10:23:00 +0200

    Add Image node
    Image is a node that displays an image given a Context and a resource id

---
+++ src/main/java/com/rotlug/glebadroid/Image.java
@@ -0,0 +1,23 @@
+package com.rotlug.glebadroid;
+
+import android.content.Context;
+import android.graphics.BitmapFactory;
+
+/*
+Image is a node that draws an image on the canvas, given a resourceId and context.
+ */
+public class Image extends Node2D {
+    Context context;
+    int resourceId;
+
+    public Image(Context context, int resourceId) {
+        this.context = context;
+        this.resourceId = resourceId;
+    }
+
+    @Override
+    public void onReady() {
+        setBitmap(BitmapFactory.decodeResource(context.getResources(), resourceId));
+        super.onReady();
+    }
+}
