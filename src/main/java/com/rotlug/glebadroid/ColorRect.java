4ed7643c1b8b09e4e9e732ccbcdefd7593aa88d6
rotlug <rotlug77@gmail.com>
2024-12-20 12:38:07 +0200

    Add ColorRect
    ColorRects is a node that can draw a colored rectangle on the screen

---
+++ src/main/java/com/rotlug/glebadroid/ColorRect.java
@@ -0,0 +1,42 @@
+package com.rotlug.glebadroid;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+/*
+ColorRect is a node that can draw a colored rectangle on the canvas (Mainly used for debugging)
+ */
+public class ColorRect extends Node2D {
+    int color;
+
+    public ColorRect(int color) {
+        this.color = color;
+    }
+
+    @Override
+    public void onReady() {
+        super.onReady();
+        setBitmap(createBitmap());
+    }
+
+    private Bitmap createBitmap() {
+        // Here you create the bound of your shape
+        Rect rect = new Rect(0, 0, 1, 1);
+
+        // You then create a Bitmap and get a canvas to draw into it
+        Bitmap image = Bitmap.createBitmap(rect.width(), rect.height(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(image);
+
+        //Paint holds information about how to draw shapes
+        Paint paint = new Paint();
+        paint.setColor(color);
+
+        // Then draw your shape
+        canvas.drawRect(rect, paint);
+
+        return image;
+    }
+}
