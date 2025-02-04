ae98d0137a9fd78be7871f1fcf3e0ca1b93c797b
rotlug <rotlug77@gmail.com>
2025-02-04 15:24:07 +0200

    Add names to nodes, make destroySelf destroy all children

--- src/main/java/com/rotlug/glebadroid/Node.java
+++ src/main/java/com/rotlug/glebadroid/Node.java
@@ -14,12 +14,14 @@ public class Node {
     private final ArrayList<Node> children;
     private GameView gameView;
     private Node parent;
+    private String name;
 
     Dictionary<String, ArrayList<Node>> signals;
 
     public Node() {
         this.children = new ArrayList<>();
         this.signals = new Hashtable<>();
+        this.name = getClass().getSimpleName();
     }
 
     public void onReady() {
@@ -93,8 +95,22 @@ public class Node {
     // Destroy node
     public void destroySelf() {
         this.onDestroy();
+        // Destroy all children
+        for (int i = 0; i < children.size(); i++) {
+            Node child = children.get(i);
+            child.destroySelf();
+        }
         getParent().getChildren().remove(this);
     }
 
     public void onDestroy() {}
+
+    // Name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
