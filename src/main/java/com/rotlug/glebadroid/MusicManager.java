7955bf65c1d931f5d028de6118bac6a4338633a2
rotlug <rotlug77@gmail.com>
2025-05-09 18:38:22 +0300

    Add Music Manager 2

--- src/main/java/com/rotlug/glebadroid/MusicManager.java
+++ src/main/java/com/rotlug/glebadroid/MusicManager.java
@@ -1,4 +1,30 @@
 package com.rotlug.glebadroid;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+
 public class MusicManager {
+    private static MediaPlayer mediaPlayer;
+
+    public static void play(Context context, int resId, float volume) {
+        stop(); // Stop any existing playback
+        mediaPlayer = MediaPlayer.create(context, resId);
+        if (mediaPlayer != null) {
+            mediaPlayer.setLooping(true); // Enable seamless looping
+            mediaPlayer.setVolume(volume, volume);
+            mediaPlayer.start();
+        }
+    }
+
+    public static void play(Context context, int resId) {
+        play(context, resId, 1);
+    }
+
+    public static void stop() {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
 }
